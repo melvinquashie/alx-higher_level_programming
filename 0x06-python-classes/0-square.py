483924e52c00d752ad58d0548531dca4919edb01
Melvin Quashie <melvinquashie@outlook.com>
2023-08-29 21:06:08 +0000

    adding documentation and making executable

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Square class"""
 
 class Square:
     """An empty class Square."""
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" Square class based on 0-square.py """
 
 class Square():
     """Class square"""
