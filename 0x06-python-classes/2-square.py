812f7fb83a10919908e73d4f9fba9aaee6ffc6ee
Melvin Quashie <melvinquashie@outlook.com>
2023-08-29 21:11:55 +0000

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square based on 1-square.py."""
+
+
+class Square():
+    """square class with it's proper size and proper validation"""
+
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
