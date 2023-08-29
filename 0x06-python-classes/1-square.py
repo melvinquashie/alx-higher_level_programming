7f330e7e4b10004794b00c306979ef0abcd5f6e9
Melvin Quashie <melvinquashie@outlook.com>
2023-08-29 20:47:25 +0000

    1-square.py and update of task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square():
+    """Class square"""
+
+    def __init__(self, size):
+        """ initialize the class
+
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,4 +1,5 @@
 # 0x06. Python - Classes and Objects
 
 * 0-square.py: an empty class Square that defines a square
+* 1-square.py: a class Square that defines a square by: (based on 0-square.py)
 * 
